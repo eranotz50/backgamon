df76b5a6f428859d4601bbc5bee92a002ce75f25
dan m <john.doe@gmail.com>
2019-12-08 20:58:11 +0200

    Web Api Controller + Create User Method

---
+++ Backgamon/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Http;
+
+namespace Backgamon.Controllers
+{
+    public class UsersController : ApiController
+    {
+        public CreateUserResponse CreateUser(CreateUserRequest request) // [FromBody]
+        {
+            var response = new CreateUserResponse();
+
+            return response;
+        }
+    }
+
+    public class CreateUserRequest
+    {
+        public string User { get; set; }
+
+        public string Password { get; set; }
+    }
+
+    public class CreateUserResponse
+    {
+        public string ErrorMessage { get; set; }
+
+        public bool IsOk { get { return string.IsNullOrEmpty(ErrorMessage); } }
+    }
+
+}
--- Backgamon/app.js
+++ Backgamon/app.js
@@ -1,22 +1,40 @@
-﻿function connect() {
+﻿
+function connect() {
     console.log('connect');
 
     var url = this.location + "/signalr";
-    var proxy = $.connection.gameHub; 
+    var proxy = $.connection.gameHub;
 
     $.connection.hub.start()
-         .done(function () {
-             console.log('Connected.');             
-             proxy.server.send('test test');
-         })
-        .fail(function (err) {
+        .done(function() {
+            console.log('Connected.');
+            proxy.server.send('test test');
+        })
+        .fail(function(err) {
             console.log('Connection Failed -> ' + err);
-        })    
+        });
 }
 
 function send() {
     console.log('send');
-    var proxy = $.connection.gameHub; 
+    var proxy = $.connection.gameHub;
 
     proxy.server.send('test test 22');
 }
+
+function createUser(user, pwd) {
+    var req = { User: user, Password: pwd };
+    $.post("/api/Users/CreateUser", req)
+        .done(function(res) {
+            console.log("User Create -> " + res);
+        })
+        .fail(function() {
+            console.log("User Create");
+        })
+        .always(function() {
+            console.log("finished");
+        });
+
+
+}
+
